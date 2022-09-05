3133389745be94c7b09756e52c458e41d1fd67ff
pcal43 <github@pcal.net>
2022-09-05 16:57:15 -0700

    meh

--- release.sh
+++ release.sh
@@ -4,5 +4,10 @@
 # Because I refuse to twist myself into knots trying to get gradle to do this.
 #
 
+CURRENT_VERSION=$(sed -rn 's/^mod_version.*=[ ]*([^\n]+)$/\1/p' gradle.properties)
+echo "Current version is '$CURRENT_VERSION'"
 
-sed -i "/mod_version=/ s/=.*/=HEY/" gradle.properties
+RELEASE_VERSION=$(echo $CURRENT_VERSION | sed s/-prerelease//)
+echo "Release version will be '$RELEASE_VERSION'"
+
+#sed -i "/mod_version=/ s/=.*/=HEY/" gradle.properties
