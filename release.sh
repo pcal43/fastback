4d7f002feb635d59b1d2b1239b46fbf6ec7ae949
pcal43 <github@pcal.net>
2022-09-05 18:25:56 -0700

    add release script

---
+++ release.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+#
+# Release a new version.
+#
+# Shell script ecause I refuse to twist myself into knots trying to get gradle to do
+# simple stuff like this.
+#
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Working directory not clean, cannot release"
+  exit 1
+fi
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+if [ "${CURRENT_BRANCH}" != 'main' ]; then
+  echo "Releases must be performed on main.  Currently on '${CURRENT_BRANCH}'"
+  exit 1
+fi
+
+CURRENT_VERSION=$(sed -rn 's/^mod_version.*=[ ]*([^\n]+)$/\1/p' gradle.properties)
+echo "Current version is '$CURRENT_VERSION'"
+
+RELEASE_VERSION=$(echo $CURRENT_VERSION | sed s/-prerelease//)
+if [ $CURRENT_VERSION = $RELEASE_VERSION ]; then
+    echo "ERROR - current version is not a prerelease: $CURRENT_VERSION"
+    exit 1
+fi
+echo "Release version will be '$RELEASE_VERSION'"
+sed -ie "s/^mod_version =.*/mod_version = $RELEASE_VERSION/" gradle.properties
+
+git commit -am "Release ${RELEASE_VERSION}"
+git tag "${RELEASE_VERSION}"
+git push --tags
+
+rm -rf build/libs
+./gradlew remapJar
+
+# this is always creating a draft release for some reason
+gh release create ${RELEASE_VERSION} build/libs/ --title "${RELEASE_VERSION}" --notes "release ${RELEASE_VERSION}"
+
+BUILD_METADATA=$(echo ${RELEASE_VERSION} | awk '{split($NF,v,/[+]/); $NF=v[2]}1')
+BUILD_METADATA="${BUILD_METADATA}-prerelease"
+NEXT_MOD_VERSION=$(echo ${RELEASE_VERSION} | awk '{split($NF,v,/[.]/); $NF=v[1]"."v[2]"."++v[3]}1')
+
+NEXT_VERSION="${NEXT_MOD_VERSION}+${BUILD_METADATA}"
+echo "Next version is ${NEXT_VERSION}"
+
+sed -ie "s/^mod_version =.*/mod_version = $NEXT_VERSION/" gradle.properties
+git commit -am "Prepare for next version ${NEXT_VERSION}"
+git push
