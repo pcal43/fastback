af1b4b1e71ae110eedaff5d125fde69fad2ee913
pcal43 <github@pcal.net>
2022-09-05 17:13:05 -0700

    checkpoint

--- release.sh
+++ release.sh
@@ -12,7 +12,14 @@ if [ $CURRENT_VERSION = $RELEASE_VERSION ]; then
     echo "ERROR - current version is not a prerelease: $CURRENT_VERSION"
     exit 1
 fi
-
 echo "Release version will be '$RELEASE_VERSION'"
-
 sed -ier "s/^mod_version =.*/mod_version = $RELEASE_VERSION/" gradle.properties
+
+git commit -am "Release ${RELEASE_VERSION}"
+
+NEXT_VERSION=$(echo RELEASE_VERSION | awk -F. '/[0-9]+\./{$NF++;print}' OFS=.)
+NEXT_VERSION="${NEXT_VERSION}-prerelease"
+echo "Next version is ${NEXT_VERSION}"
+
+sed -ier "s/^mod_version =.*/mod_version = $NEXT_VERSION/" gradle.properties
+git commit -am "Prepare for next version ${NEXT_VERSION}"
