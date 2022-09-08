cc02467029ef90b44c0d828e7a1c8aab0e86eab6
Patrick Calahan <github@pcal.net>
2022-09-07 18:39:26 -0700

    checkpoint

--- src/main/java/net/pcal/fastback/commands/Commands.java
+++ src/main/java/net/pcal/fastback/commands/Commands.java
@@ -9,6 +9,7 @@ public class Commands {
 
     public static void registerCommands(final ModContext mctx, final String fastbackCommand) {
         final LiteralArgumentBuilder<ServerCommandSource> fastbackCmd = LiteralArgumentBuilder.literal(fastbackCommand);
+        EnableCommand.register(fastbackCmd, mctx);
         StatusCommand.register(fastbackCmd, mctx);
         RestoreCommand.register(fastbackCmd, mctx);
         ListCommand.register(fastbackCmd, mctx);
---
+++ src/main/java/net/pcal/fastback/commands/EnableCommand.java
@@ -0,0 +1,52 @@
+package net.pcal.fastback.commands;
+
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.ServerCommandSource;
+import net.pcal.fastback.Loggr;
+import net.pcal.fastback.ModContext;
+import net.pcal.fastback.WorldConfig;
+import net.pcal.fastback.tasks.TaskListener;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.lib.StoredConfig;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import static java.util.Objects.requireNonNull;
+
+public class EnableCommand {
+
+    public static void register(final LiteralArgumentBuilder<ServerCommandSource> fastbackCmd, final ModContext ctx) {
+        final EnableCommand rc = new EnableCommand(ctx);
+        fastbackCmd.then(CommandManager.literal("enable").executes(rc::execute));
+    }
+
+    private final ModContext ctx;
+
+    private EnableCommand(ModContext context) {
+        this.ctx = requireNonNull(context);
+    }
+
+    private int execute(final CommandContext<ServerCommandSource> cc) {
+        final MinecraftServer server = cc.getSource().getServer();
+        final TaskListener taskListener = new ServerTaskListener(server.getCommandSource());
+        final Loggr logger = this.ctx.getLogger();
+        final Path worldSaveDir = this.ctx.getWorldSaveDirectory(server);
+        try (final Git git = Git.open(worldSaveDir.toFile())) {
+            WorldConfig.ensureWorldHasUuid(worldSaveDir, logger);
+            final StoredConfig config = git.getRepository().getConfig();
+            WorldConfig.setBackupEnabled(config, true);
+            WorldConfig.setShutdownBackupEnabled(config, true);
+            config.save();
+            taskListener.feedback("Enabled automatic local backups on world shutdown.");
+            return 1;
+        } catch (IOException e) {
+            taskListener.internalError();
+            logger.error("error enabling backups", e);
+            return 0;
+        }
+    }
+}
