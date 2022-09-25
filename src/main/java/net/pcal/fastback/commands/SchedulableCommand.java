12a96fc8d20249113a9d0728e2373b67e3f6fe22
pcal43 <github@pcal.net>
2022-09-25 13:27:37 -0700

    checkpoint

---
+++ src/main/java/net/pcal/fastback/commands/SchedulableCommand.java
@@ -0,0 +1,55 @@
+/*
+ * FastBack - Fast, incremental Minecraft backups powered by Git.
+ * Copyright (C) 2022 pcal.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pcal.fastback.commands;
+
+
+import net.minecraft.server.command.ServerCommandSource;
+import net.pcal.fastback.ModContext;
+import net.pcal.fastback.logging.Logger;
+
+import static java.util.Objects.requireNonNull;
+
+
+public enum SchedulableCommand  {
+
+    LOCAL("local") {
+
+        @Override
+        public void run(ModContext ctx, ServerCommandSource scs, Logger log) {
+            log.info("Starting scheduled local backup");
+            //new CommitTask(git, ctx, server, log).run();
+        }
+    };
+
+    public static SchedulableCommand getForConfigKey(String configKey) {
+        return LOCAL; //FIXME
+    }
+
+    private final String configKey;
+
+    SchedulableCommand(String configKey) {
+        this.configKey = requireNonNull(configKey);
+    }
+
+    public String getConfigKey() {
+        return this.configKey;
+    }
+
+    public abstract void run(ModContext ctx, ServerCommandSource scs, Logger log);
+}
---
+++ src/main/java/net/pcal/fastback/commands/SetShutdownCommandCommand.java
@@ -0,0 +1,59 @@
+/*
+ * FastBack - Fast, incremental Minecraft backups powered by Git.
+ * Copyright (C) 2022 pcal.net
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package net.pcal.fastback.commands;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.server.command.ServerCommandSource;
+import net.pcal.fastback.ModContext;
+import net.pcal.fastback.WorldConfig;
+import org.eclipse.jgit.lib.StoredConfig;
+
+import static net.minecraft.server.command.CommandManager.argument;
+import static net.minecraft.server.command.CommandManager.literal;
+import static net.pcal.fastback.commands.Commands.SUCCESS;
+import static net.pcal.fastback.commands.Commands.executeStandardNew;
+import static net.pcal.fastback.commands.Commands.subcommandPermission;
+
+public class SetShutdownCommandCommand {
+
+    private static final String COMMAND_NAME = "set-shutdown-command";
+    private static final String ARGUMENT = "command";
+
+    public static void register(final LiteralArgumentBuilder<ServerCommandSource> argb, final ModContext ctx) {
+        argb.then(
+                literal(COMMAND_NAME).
+                        requires(subcommandPermission(ctx, COMMAND_NAME)).then(
+                                argument(ARGUMENT, StringArgumentType.greedyString()).
+                                        executes(cc->execute(ctx, cc)))
+        );
+    }
+
+    public static int execute(final ModContext ctx, final CommandContext<ServerCommandSource> cc) throws CommandSyntaxException {
+        return executeStandardNew(ctx, cc.getSource(), (git, wc, log) -> {
+            final String commands = cc.getArgument(ARGUMENT, String.class);
+            final StoredConfig config = git.getRepository().getConfig();
+            WorldConfig.setShutdownCommands(config, commands);
+            config.save();
+            return SUCCESS;
+        });
+    }
+}
