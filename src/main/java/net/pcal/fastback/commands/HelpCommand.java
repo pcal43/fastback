fa232a7af7f9d5797d607671891349a329c96097
pcal43 <github@pcal.net>
2022-09-26 02:14:35 -0700

    fix command help strings (#110)

--- src/main/java/net/pcal/fastback/commands/HelpCommand.java
+++ src/main/java/net/pcal/fastback/commands/HelpCommand.java
@@ -142,18 +142,18 @@ public class HelpCommand {
 
     private static void writeMarkdownReference(CommandContext<ServerCommandSource> cc, PrintWriter out) {
         out.println();
-        out.println("Command            | Use");
-        out.println("------------------ | ---");
+        out.println("Command                | Use");
+        out.println("---------------------- | ---");
         for (final String sub : getSubcommandNames(cc)) {
             //FIXME GROSS.  HOW DO WE LOCALIZE WITHOUT GOING THROUGH minecraft.text?
             net.minecraft.text.Text shortHelp = net.minecraft.text.Text.translatable("commands.fastback.help." + sub);
-            String paddedSub = String.format("%-" + 18 + "s", "`" + sub + "`");
+            String paddedSub = String.format("%-" + 22 + "s", "`" + sub + "`");
             out.println(paddedSub + " | " + shortHelp.getString());
         }
         out.println();
         out.println("Permission                       ");
         out.println("-------------------------------- ");
-        out.println("`" + BACKUP_COMMAND_PERM + "`");
+        out.println("* `" + BACKUP_COMMAND_PERM + "`");
         for (final String sub : getSubcommandNames(cc)) {
             String permName = subcommandPermName(sub);
             String paddedPerm = String.format("%-" + 32 + "s", "`" + permName + "`");
