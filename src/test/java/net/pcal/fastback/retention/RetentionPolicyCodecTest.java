8969b486fe53e87984edd0f5da3be862247764dc
Patrick Calahan <github@pcal.net>
2023-02-23 00:47:41 -0800

    checkpoint

--- src/test/java/net/pcal/fastback/retention/RetentionPolicyCodecTest.java
+++ src/test/java/net/pcal/fastback/retention/RetentionPolicyCodecTest.java
@@ -44,7 +44,7 @@ public class RetentionPolicyCodecTest {
         assertEquals("mock-policy baz=bop foo=bar", encodedPolicy);
     }
 
-    @org.junit.Test
+    @Test
     public void testDecodePolicy() {
         final RetentionPolicy policy = RetentionPolicyCodec.INSTANCE.decodePolicy(
                 MockModContext.create(),
