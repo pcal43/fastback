5228220f7c8c803f523304fab029a35bb352815e
pcal43 <github@pcal.net>
2023-06-28 19:53:59 -0700

    bump JGit to 6.6.0 (#206)

--- src/main/java/net/pcal/fastback/tasks/GcTask.java
+++ src/main/java/net/pcal/fastback/tasks/GcTask.java
@@ -164,5 +164,9 @@ public class GcTask implements Callable<Void> {
         public void progressDone(String task) {
             logger.info("Done " + task);
         }
+
+        @Override
+        public void showDuration(boolean enabled) {
+        }
     }
 }
